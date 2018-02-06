ee11a3ee8dbca7b896caa282ad7db8462fc01552
Jesper Lökfors <jesper.lokfors@controlthings.fi>
2018-02-06 08:09:10 +0200

    removed armeabi and changed platform to 16 same as rest of app

--- Application.mk
+++ Application.mk
@@ -1,3 +1,4 @@
 #armeabi-v7a added because importing 'realm' to the project 
 #arm64-v8a because KBä's telephone is of that arch
-APP_ABI := armeabi armeabi-v7a arm64-v8a x86_64
+APP_ABI := armeabi-v7a arm64-v8a x86_64
+APP_PLATFORM := android-16
